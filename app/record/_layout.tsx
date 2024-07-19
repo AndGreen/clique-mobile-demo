f1205b46bbc237cce1bda41f796c914ab1545443
Andrew Zelenin <zelenin.andrew@gmail.com>
2024-07-20 00:52:56 +0300

    Add providers, rewrite audio provider

--- app/record/_layout.tsx
+++ app/record/_layout.tsx
@@ -1,13 +1,13 @@
-import {RecorderWidgetProvider} from "@/components/RecorderWidget/RecorderWidgetContext";
-import {AudioPlayerProvider} from "@/components/AudioPlayer/AudioPlayerContext";
-import {Stack} from "expo-router";
+import { RecorderWidgetProvider } from '@/components/RecorderWidget/RecorderWidgetContext';
+import { AudioPlayerProvider } from '@/components/AudioPlayer/AudioPlayerContext';
+import { Stack } from 'expo-router';
 
 export default function RecordLayout() {
     return (
-        // <RecorderWidgetProvider>
-        //     <AudioPlayerProvider>
-                <Stack screenOptions={{ headerShown: false }}/>
-            // </AudioPlayerProvider>
-        // </RecorderWidgetProvider>
+        <RecorderWidgetProvider>
+            <AudioPlayerProvider>
+                <Stack screenOptions={{ headerShown: false }} />
+            </AudioPlayerProvider>
+        </RecorderWidgetProvider>
     );
 }
--- components/AudioPlayer/AudioPlayerContext.js
+++ components/AudioPlayer/AudioPlayerContext.js
@@ -1,86 +1,73 @@
 import React, { createContext, useContext, useRef, useState, useEffect } from 'react';
 import AudioPlayer from './AudioPlayer';
+import { Audio } from 'expo-av';
 
 export const AudioPlayerContext = createContext();
 
 export const useAudioPlayer = () => useContext(AudioPlayerContext);
 
 export const AudioPlayerProvider = ({ children }) => {
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [currentUrl, setCurrentUrl] = useState('');
-  const [visible, setVisible] = useState(false);
-  const audioRef = useRef(new Audio());
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [currentUrl, setCurrentUrl] = useState('');
+    const [visible, setVisible] = useState(false);
+    const audioRef = useRef(new Audio.Sound());
 
-  useEffect(() => {
-    if (isPlaying) {
-      setVisible(true); // Показывается плеер при воспроизведении
-    }
-  }, [isPlaying]);
+    useEffect(() => {
+        if (isPlaying) {
+            setVisible(true); // Показывается плеер при воспроизведении
+        }
+    }, [isPlaying]);
 
-  // Функция для воспроизведения аудио
-  const play = url => {
-    const audio = audioRef.current;
-    if (audio.src !== url) {
-      audio.src = url;
-      audio.load();
-    }
-    audio
-      .play()
-      .then(() => {
+    // Функция для воспроизведения аудио
+    const play = async (url) => {
+        const { sound } = await Audio.Sound.createAsync({ uri: url }, { shouldPlay: true });
+
+        audioRef.current = sound;
         setIsPlaying(true);
         setCurrentUrl(url);
-      })
-      .catch(error => console.error('Error playing audio:', error));
-  };
 
-  // Функция для приостановки воспроизведения
-  const pause = () => {
-    const audio = audioRef.current;
-    audio.pause();
-    setIsPlaying(false);
-  };
-
-  // Функция для остановки воспроизведения и сброса стейта
-  const stop = () => {
-    const audio = audioRef.current;
-    audio.pause();
-    audio.currentTime = 0;
-    setIsPlaying(false);
-    setCurrentUrl('');
-    setVisible(false);
-  };
-
-  // Отслеживание окончания воспроизведения трека
-  useEffect(() => {
-    const audio = audioRef.current;
-
-    const handleEnded = () => {
-      setIsPlaying(false);
-      // setCurrentUrl(''); // Не сбрасывать url аудио
-      audio.currentTime = 0; // Сбросить время на начало
+        sound.setOnPlaybackStatusUpdate((status) => {
+            if (status.didJustFinish) {
+                handleEnded();
+            }
+        });
     };
 
-    audio.addEventListener('ended', handleEnded);
-
-    return () => {
-      audio.removeEventListener('ended', handleEnded);
+    // Функция для приостановки воспроизведения
+    const pause = async () => {
+        const sound = audioRef.current;
+        await sound.pauseAsync();
+        setIsPlaying(false);
     };
-  }, []);
 
-  const value = {
-    isPlaying,
-    currentUrl,
-    visible,
-    audioRef,
-    play,
-    pause,
-    stop,
-  };
+    // Функция для остановки воспроизведения и сброса стейта
+    const stop = async () => {
+        const sound = audioRef.current;
+        await sound.stopAsync();
+        setIsPlaying(false);
+        setCurrentUrl('');
+        setVisible(false);
+    };
 
-  return (
-    <AudioPlayerContext.Provider value={value}>
-      {children}
-      {/*<AudioPlayer />*/}
-    </AudioPlayerContext.Provider>
-  );
+    const handleEnded = async () => {
+        setIsPlaying(false);
+        await soundRef.current.setPositionAsync(0);
+    };
+
+    const value = {
+        isPlaying,
+        currentUrl,
+        visible,
+        audioRef,
+        play,
+        pause,
+        stop,
+    };
+
+    return (
+        <AudioPlayerContext.Provider value={value}>
+            {children}
+            {/*<AudioPlayer />*/}
+        </AudioPlayerContext.Provider>
+    );
 };
